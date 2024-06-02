1ff5a04404ad34baf420f35cbcd2978e274b5b41
AliHashempour <ali.hashempour@gmail.com>
2024-06-02 11:20:47 -0700

    refactore: basket hanlder rename

--- api/handler/Basket.go
+++ api/handler/Basket.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"basket/internal/database"
 	"basket/internal/model"
 	"encoding/json"
 	"github.com/labstack/echo/v4"
@@ -23,10 +22,10 @@ func BasketAdd(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
 	}
-	result := database.DB.Create(&basket)
-	if result.Error != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": result.Error.Error()})
-	}
+	//result := database.DB.Create(&basket)
+	//if result.Error != nil {
+	//	return c.JSON(http.StatusInternalServerError, map[string]string{"error": result.Error.Error()})
+	//}
 
 	return c.JSON(http.StatusCreated, map[string]interface{}{"data": basket})
 }
