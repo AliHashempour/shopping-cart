e41d0408be46ec178c0e553c8976284f75625834
AliHashempour <ali.hashempour@gmail.com>
2024-06-03 16:05:22 -0700

    fix: userID typo

--- internal/model/user.go
+++ internal/model/user.go
@@ -6,5 +6,5 @@ type User struct {
 	gorm.Model
 	Username string    `json:"username" gorm:"unique"`
 	Password string    `json:"password"`
-	Baskets  []*Basket `json:"baskets"  gorm:"foreignKey:UserID"`
+	Baskets  []*Basket `json:"baskets"  gorm:"foreignKey:UserId"`
 }
